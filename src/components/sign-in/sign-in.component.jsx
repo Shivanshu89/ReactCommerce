720b44d09744f32f8257af406eef332a2647aff6
Shivanshu Misra <shivanshu.misra-acn@hpe.com>
2021-07-27 17:15:12 +0530

    finishing sign in handle submit message

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-buttom/custom-buttom.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 import './sign-in.styles.scss';
 
 
@@ -15,14 +15,20 @@ class SignIn extends Component {
         }
     }
 
-    handleSubmit = event =>{
+    handleSubmit = async event =>{
         event.preventDefault();
-        this.state({email:'', password:''});
+        const {email, password} = this.state;
+        try{
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email:'', password:''});
+        }catch(error){
+            console.log(error);
+        }
     }
 
     handleChange = event =>{
         const {value, name} = event.target;
-        this.state({[name]: value});
+        this.setState({[name]: value});
     }
     render(){
         return(
