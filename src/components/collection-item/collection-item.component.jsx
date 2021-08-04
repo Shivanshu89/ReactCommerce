24bbec33e34c1fd0c637cf8775f02d8cc87ebcbc
Shivanshu Misra <shivanshu.misra-acn@hpe.com>
2021-08-04 15:35:16 +0530

    updated shop data to new data and added cart utils to group items in cart reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions';
 import './collection-item.styles.scss';
 import CustomButton from '../custom-buttom/custom-buttom.component';
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
-    <div className="collection-item">
-        <div  className='image'style={{ backgroundImage: `url(${imageUrl})`}}>
+const CollectionItem = ({item, addItem }) => {
+    const { name, price, imageUrl} = item;
+    return(
+        <div className="collection-item">
+            <div  className='image'style={{ backgroundImage: `url(${imageUrl})`}}>
+            </div>
+            <div className='collection-footer'>
+                <span className='name'>{name}</span>
+                <span className='price'>{price}</span>
+            </div>
+            <CustomButton onClick={() => addItem(item)} inverted>Add to cart</CustomButton>
         </div>
-        <div className='collection-footer'>
-            <span className='name'>{name}</span>
-            <span className='price'>{price}</span>
-        </div>
-        <CustomButton inverted>Add to cart</CustomButton>
-    </div>
-);
+    );
+}
 
-export default CollectionItem;
+//means when addItem function is called it will add item as a props and dispatch the item via addItem action
+const mapDispatchToProps = dispatch =>({
+    addItem : item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
